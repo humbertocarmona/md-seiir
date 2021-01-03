4f2bda7ebec9e4e7d9b12eea48f6011f54c0391f
humberto <carmona@fisica.ufc.br>
2021-01-03 10:50:48 -0300

    reset button + blue noise

--- sketch.js
+++ sketch.js
@@ -29,11 +29,15 @@ function setup() {
 
 
   
-  world = new World(10,10,500,500);
-  world.addParticles(15,15);  
-  
+  world = new World(10,10,600);
+  world.addParticles(0.5);
+
+
   runButton = createButton("play");
 	runButton.mousePressed(togglePlaying);
+  
+  runButton = createButton("reset");
+  runButton.mousePressed(resetCall);
 
 	runButton = createButton("controls");
   runButton.mousePressed(toggleControls);
@@ -49,6 +53,14 @@ function draw() {
   world.update();  
   world.show(); 
   if (frameCount>100  && !world.running) noLoop();
+}
+
+function resetCall(){
+  world.particles = [];
+  world.clickDistancingParticles = [];
+
+  world.addParticles(0.5);
+  draw();
 
 }
 
@@ -72,11 +84,9 @@ function mousePressed() {
 
 }
 
-
-
 //world class
 class World{
-  constructor(x,y,w,h){
+  constructor(x,y,w){
     this.t = 0;
     this.dt = 1;
     this.particles = [];
@@ -89,21 +99,27 @@ class World{
     this.D = 0;
     this.running = true;
     this.width=w;
-    this.height=h;
+    this.height=w;
     this.x = x;
     this.y = y;
-    this.cellWidth = 25;
-    this.cellHeight = 25;
-    this.nCellsX = int(this.width/this.cellWidth);
-    this.nCellsY = int(this.height/this.cellWidth);
-    this.cellList = Array(this.nCellsX*this.nCellsY);
-    this.ncells = this.nCellsX*this.nCellsY;
-    this.offset = [[0,0] , [1,0], [-1,1], [0,1],[1,1]];
-    this.clickDistancingParticles = [];
-    this.rc = createVector(this.x+this.width+250,200)
-    this.cylRadius = 150;
 
+    this.cellWidth = 25;
+    this.nCellsX = int(this.width/this.cellWidth);
+    this.cellList = Array(this.nCellsX*this.nCellsX);
+    this.ncells = this.nCellsX*this.nCellsX;
+    this.offset = [[0,0] , [1,0], [-1,1], [0,1],[1,1]];
+    
+    console.log(this.nCellsX, this.nCellsX,this.ncells);
+    
+    this.clickDistancingParticles = [];
+    this.distancingRadius = 0.4*(width-this.width-this.x);
+    var xc = this.x + this.width + this.distancingRadius + 0.05*this.distancingRadius;
+    var yc = this.y + 0.25*this.height + this.distancingRadius;
+    this.rc = createVector(xc,yc)
+    
   }
+
+  
 
   updateCellList(){
     for (let i = 0; i < this.ncells; i++) {
@@ -112,8 +128,8 @@ class World{
       
     this.particles.forEach(p => {
       let j = floor((p.position.x - this.x)/this.cellWidth);
-      let i = floor((p.position.y - this.y)/this.cellHeight);
-      if (0 <= i && i<this.nCellsY && 0 <= j && j<this.nCellsX ){
+      let i = floor((p.position.y - this.y)/this.cellWidth);
+      if (0 <= i && i<this.nCellsX && 0 <= j && j<this.nCellsX ){
         let c = j + i*this.nCellsX
         p.cell = c;
         this.cellList[c].push(p.n); 
@@ -125,7 +141,118 @@ class World{
     this.particles.push(p);
   }
 
-  addParticles(nx, ny){
+  addParticles2(density=0.2){
+      var diam = 25;
+      var parea = Math.PI*diam*diam/4;
+      var warea = this.width*this.height;
+      var nparticles = int(density*warea/parea);
+      var minSep = 60;
+      let vmean = 0.5;
+      let vsd = 0.5;
+
+      console.log('minSep=',minSep.toFixed(2))
+
+      // initialize  Poisson Disk sampling
+      // https://www.cs.ubc.ca/~rbridson/docs/bridson-siggraph07-poissondisk.pdf
+      var kmax = 10;
+      var grid = [];
+      var active = [];
+      var gridWidth = 0.5*minSep/sqrt(2);
+      var gridCols = floor(this.width/gridWidth)
+      var gridRows = floor(this.height/gridWidth)
+      for (var i = 0; i< gridCols*gridRows; i++) grid[i] = -1;  
+      
+
+      //step 1 - add seed particle
+      var x = random(this.x, this.x + this.width);
+      var y = random(this.y, this.y + this.height);
+      var i = floor((x-this.x)/gridWidth );
+      var j = floor((y-this.y)/gridWidth );
+      var c = i + j*gridCols;
+      var pos = createVector(x,y);
+
+      grid[c] = pos; 
+      active.push(pos)
+      
+      // step 2 - adding new particles
+      var n = 1;
+      while (active.length >0 && n<40 ){
+
+          // 2.1 - select a random particle from the active list
+          var rindex = floor(random(active.length));
+          var posA = active[rindex]; 
+          // 2.2 generate kmax samples
+          var ok = false;
+          var k = 0;         
+          while(!ok || k == kmax){
+              var sample = p5.Vector.random2D();
+              var m = random(minSep, 2*minSep);
+              sample.setMag(m); 
+              sample.add(posA);
+
+              if (sample.x < this.x || sample.x > (this.x+this.width)) {k++;continue;}
+              if (sample.y < this.y || sample.y > (this.y+this.height)) {k++;continue;}
+              var is = floor((sample.x - this.x)/gridWidth );
+              var js = floor((sample.y - this.y)/gridWidth );
+              var cs = is + js*gridCols;
+              // check distance from previous particles
+              ok = true;
+              for (var io = -1; io<=1; io++){
+                var i1 = (is+io);
+                for (var jo = -1; jo<=1; jo++){
+                      var j1 = (js+jo);
+                      var co = i1 + j1*gridCols;
+                      var neighbor = grid[co];
+                      if (!neighbor) continue;
+                      if (neighbor != -1){
+                           var dist = p5.Vector.dist(sample, neighbor);
+                           if (dist < minSep){
+                             ok = false;
+                           }
+                       }
+                  }
+              }
+              if (ok){
+                 grid[cs] = sample;
+                 active.push(sample);
+                 n++;
+              }
+              k++ 
+          }
+          if (!ok){
+            // remove point from list
+            active.splice(rindex,1);
+          }
+      }
+
+      n = 0;
+      for (let i = 0; i < grid.length; i++) {
+        const pos = grid[i];
+        if(pos && pos!=-1){
+          let p = new Particle(pos.x,pos.y);
+          let v = p5.Vector.random2D();
+          let vmag = randomGaussian(vmean, vsd);
+          v.setMag(vmag);
+          p.setVelocity(v.x,v.y);
+          p.n = n;
+          this.addParticle(p);
+          n++;
+        }
+      }
+      this.updateCellList();
+  }
+
+  addParticles(density=0.2){
+
+    var r = this.width/this.height;
+    var diam = 25;
+    var parea = Math.PI*diam*diam/4;
+    var nx = int(sqrt(density*this.width*this.width/parea))
+    var ny = int(nx*r)
+    print('nxx='+nx, 'nyy='+ny)
+
+    //  r  = w/h = nx/ny
+    // (nx*nx*pi*diam^2/4)/ = dens*w*w
     
     let dx = this.width/nx;
     let dy = this.height/ny;
@@ -133,9 +260,9 @@ class World{
     let vsd = 0.5;
     let n = 0;
     for (let j = 0; j< ny; j++){
-      let y = this.y + (j+0.5)*dy + random(0,0.*dy);; 
+      let y = this.y + (j+0.5)*dy + random(0,0.5*dy);; 
       for (let i = 0; i < nx; i++) {
-        let x = this.x +(i+0.5)*dx + random(0,0.*dx); 
+        let x = this.x +(i+0.5)*dx + random(0,0.5*dx); 
         let p = new Particle(x,y);
         let v = p5.Vector.random2D();
         let vmag = randomGaussian(vmean, vsd);
@@ -146,12 +273,19 @@ class World{
         n++;
       }
     }
-    for (let i = 0; i< int(0.01*this.particles.length); i++){
+    for (let i = 0; i< 1; i++){
       let n = round(random(0,this.particles.length-1));
       this.particles[n].s = 'E';
     }
-    this.updateCellList();
 
+
+    this.updateCellList();
+    for(let i = 1; i<50; i++) this.update();
+    for (let i = 0; i< this.particles.length; i++){
+        this.particles[i].timeHealing = 0;
+        this.particles[i].timeIncubation = 0;
+    }
+    this.t = -1;
   }
   
   wallCollision(){
@@ -180,7 +314,7 @@ class World{
   clickDistancingForce(){
       for(let k = 0; k < this.clickDistancingParticles.length; k++){
           let i = this.clickDistancingParticles[k];
-          var maxR = this.cylRadius - 0.5*this.particles[i].diam
+          var maxR = this.distancingRadius - 0.5*this.particles[i].diam
           var dr = p5.Vector.sub(this.particles[i].position,this.rc);
           var drmagsq = dr.magSq()
           if ( drmagsq >= maxR*maxR){
@@ -232,18 +366,19 @@ class World{
 
   elasticCollisions(){
       // run over all cells
+
       for(let c1=0; c1< this.ncells; c1++){
           let i1 = int(c1 / this.nCellsX);
           let j1 = c1 - i1*this.nCellsX;
+          
           for (let s=0; s< this.offset.length; s++){
                let shift = this.offset[s];
                let i2 = i1 + shift[1];
                let j2 = j1 + shift[0];
 
-               if (i2 < 0) continue;
-               if (j2 < 0) continue;
-               if (i2 >= this.nCellsX) continue;
-               if (j2 >= this.nCellsY) continue;
+               if (0 > i2 || i2 >= this.nCellsX) continue;
+               if (0 > j2 || j2 >= this.nCellsX) continue;
+
 
               let c2 = j2 + i2*this.nCellsX;
 
@@ -418,7 +553,7 @@ class World{
   
   clickDistancing(mx,my){
     let j = floor((mx - this.x)/this.cellWidth);
-    let i = floor((my - this.y)/this.cellHeight);
+    let i = floor((my - this.y)/this.cellWidth);
     let c = j + i*this.nCellsX
     let p = -1;
     let dist = 900;
@@ -430,7 +565,7 @@ class World{
             if (i2 < 0) continue;
             if (j2 < 0) continue;
             if (i2 >= this.nCellsX) continue;
-            if (j2 >= this.nCellsY) continue;
+            if (j2 >= this.nCellsX) continue;
             let c2 = j2 + i2*this.nCellsX;
             for (let l = this.cellList[c2].length-1; l>0 ; l-- ){
               let il = this.cellList[c2][l];
@@ -449,7 +584,7 @@ class World{
 
   distancingParticle(n){
     if (0<=n && n < this.particles.length){
-      if (this.particles[n].s === 'IR'){
+      if (this.particles[n].s === 'IR' ||this.particles[n].s === 'IU' ){
         this.particles[n].isolated = true;
         this.particles[n].position.set(this.rc.x, this.rc.y);
         this.clickDistancingParticles.push(n)
@@ -466,7 +601,7 @@ class World{
 
     if (this.clickDistancingParticles.length >0){
       fill(200,150,150,100)
-      ellipse(this.rc.x, this.rc.y, 2*this.cylRadius, 2*this.cylRadius)
+      ellipse(this.rc.x, this.rc.y, 2*this.distancingRadius, 2*this.distancingRadius)
     }
     for (let i = 0; i < this.particles.length; i++) {
       const p =  this.particles[i]
@@ -497,17 +632,17 @@ class World{
     // for (let i = 0; i < this.nCellsX; i++){
     //   line(this.x + i*this.cellWidth, this.y, this.x + i*this.cellWidth, this.y+this.height);
     // }
-    // for (let i = 0; i < this.nCellsY; i++){
-    //   line(this.x, this.y+i*this.cellHeight, this.x+this.width, this.y+i*this.cellHeight);
+    // for (let i = 0; i < this.nCellsX; i++){
+    //   line(this.x, this.y+i*this.cellWidth, this.x+this.width, this.y+i*this.cellWidth);
     // }
 
     // strokeWeight(0);
     // fill(255,0,0,150);
     // textAlign(CENTER,CENTER);
     // for (let i = 0; i < this.nCellsX; i++){
-    //     for (let j = 0; j < this.nCellsY; j++){
+    //     for (let j = 0; j < this.nCellsX; j++){
     //       let x = (j+1)*this.cellWidth;
-    //       let y = (i+1)*this.cellHeight;
+    //       let y = (i+1)*this.cellWidth;
     //       let c = j + i*this.nCellsX
     //       text(''+c, x,y)
     //     }
@@ -519,12 +654,12 @@ class World{
 
 // particle class
   class Particle{
-    constructor(x, y){
+    constructor(x, y, diam=25){
       // all in pixel units
       this.position = createVector(x, y);
       this.velocity = createVector(0.0, 0.0);
       this.acceleration = createVector(0.0, 0.0);
-      this.diam = 25;
+      this.diam = diam;
       this.s = 'S';
       this.timeHealing = 0;
       this.timeIncubation = 0;
